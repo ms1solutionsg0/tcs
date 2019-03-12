3df314a209ba9c2afe6c5f4750d9f9bcd6b51a80
Neal Bharucha <neal.bharucha@msichicago.org>
2019-03-12 14:48:28 -0500

    MMTR-26: refactor

--- client/src/js/view/components/settings/settings-general.js
+++ client/src/js/view/components/settings/settings-general.js
@@ -1,8 +1,8 @@
 import { h } from 'hyperapp';
 
-export const SettingsGeneral = ({ system }) =>
+export const SettingsGeneral = ({ actions }) =>
     <div class="settings_content">
-        <button type='button' class='button' onclick={() => system.shutdown()}>SHUTDOWN</button>
-        <button type='button' class='button' onclick={() => system.reboot()}>REBOOT</button>
+        <button type='button' class='button' onclick={() => actions.shutdown()}>SHUTDOWN</button>
+        <button type='button' class='button' onclick={() => actions.reboot()}>REBOOT</button>
         <button type='button' class='button' onclick={() => location.reload()}>RESTART STREAM</button>
     </div>;
