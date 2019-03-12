6da04a998497efd8a51e1ee534d708108b32cbb3
Neal Bharucha <neal.bharucha@msichicago.org>
2019-03-12 10:24:38 -0500

    MMTR-26: make General Admin Buttons on click.

--- client/src/js/view/components/settings/settings-general.js
+++ client/src/js/view/components/settings/settings-general.js
@@ -1,9 +1,8 @@
 import { h } from 'hyperapp';
-import { ButtonHold } from '../elements/button-hold';
 
 export const SettingsGeneral = ({ actions }) =>
     <div class="settings_content">
-        <ButtonHold text='SHUTDOWN' setValue={() => actions.system.shutdown()} />
-        <ButtonHold text='REBOOT' setValue={() => actions.system.reboot()} />
-        <ButtonHold text='Restart stream' setValue={() => actions.stream.start()} />
+        <button type='button' class='button' onClick={() => actions.system.shutdown()}>SHUTDOWN</button>
+        <button type='button' class='button' onClick={() => actions.system.reboot()}>REBOOT</button>
+        <button type='button' class='button' onClick={() => actions.stream.start()}>RESTART STREAM</button>
     </div>;
