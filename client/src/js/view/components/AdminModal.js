763628435d0fbdbb15445a8cf4d614682dbe2d66
Neal Bharucha <neal.bharucha@msichicago.org>
2019-03-15 10:36:59 -0500

    MMTR-34: Timeout reset. use oninput not onchange

--- client/src/js/view/components/AdminModal.js
+++ client/src/js/view/components/AdminModal.js
@@ -3,6 +3,7 @@ import keyboardJS from "keyboardjs";
 
 const ADMIN_PINS = ["1337", "1940", "1985"];
 const MODAL_TIMEOUT = 30000;
+let TIMEOUT;
 
 const onClickAdmin = ( msiAdmin, setMsiAdmin, msiAdminPending, setMsiAdminPending ) => {
   const pass = document.getElementById("password").value;
@@ -12,9 +13,19 @@ const onClickAdmin = ( msiAdmin, setMsiAdmin, msiAdminPending, setMsiAdminPendin
   setMsiAdminPending(!msiAdminPending);
 };
 
-const timeoutModal = setMsiAdminPending => {
-  setTimeout(() => setMsiAdminPending(false), MODAL_TIMEOUT);
+const setModalTimeout = setMsiAdminPending => {
+  TIMEOUT = setTimeout(() => setMsiAdminPending(false), MODAL_TIMEOUT);
 };
+
+const clearModalTimeout = (done) => {
+  clearTimeout(TIMEOUT);
+  done && done();
+};
+
+const resetModalTimeout = (setMsiAdminPending) => {
+  clearModalTimeout();
+  setModalTimeout(setMsiAdminPending);
+}
 
 export default function AdminModal({ msiAdminPending, setMsiAdminPending, msiAdmin, setMsiAdmin }) {
   keyboardJS.bind("enter", function (e) {
@@ -29,15 +40,15 @@ export default function AdminModal({ msiAdminPending, setMsiAdminPending, msiAdm
 
   return (
     msiAdminPending && (
-      <div className="modal" oncreate={() => timeoutModal(setMsiAdminPending)}>
+      <div className="modal" oncreate={() => setModalTimeout(setMsiAdminPending)} onremove={(element, done) => clearModalTimeout(done)} >
         <div className="modal--background" />
         <div className="modal--content">
           <div class="admin-box">
             <h2>
               Login for <bold>Administrators</bold> only
             </h2>
-            <form class="admin-box__register-form">
-              <input type="password" placeholder="pin/password" id="password" />
+            <form class="admin-box__register-form" >
+              <input type="password" placeholder="pin/password" id="password" oncreate={element => element.focus()} oninput={() => resetModalTimeout(setMsiAdminPending)} />
               <button
                 type="button"
                 class="admin-box__register-form--button"
