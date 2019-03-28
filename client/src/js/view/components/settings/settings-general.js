aec5d3d8a0d73838b0c0bea835418db9dad568e6
Jonathan Feng <the.j.feng@gmail.com>
2019-03-27 23:13:36 -0500

    MMTR-47 try restarting stream by code

--- client/src/js/view/components/settings/settings-general.js
+++ client/src/js/view/components/settings/settings-general.js
@@ -2,7 +2,7 @@ import { h } from 'hyperapp';
 
 export const SettingsGeneral = ({ actions }) =>
     <div class="settings_content">
-        <button type='button' class='button' onclick={() => actions.shutdown()}>SHUTDOWN</button>
-        <button type='button' class='button' onclick={() => actions.reboot()}>REBOOT</button>
-        <button type='button' class='button' onclick={() => location.reload()}>RESTART STREAM</button>
+        <button type='button' class='button' onclick={() => actions.system.shutdown()}>SHUTDOWN</button>
+        <button type='button' class='button' onclick={() => actions.system.reboot()}>REBOOT</button>
+        <button type='button' class='button' onclick={() => actions.stream.reconnect()}>RESTART STREAM</button>
     </div>;
