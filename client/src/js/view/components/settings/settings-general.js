060a1ebaaa37288d3ec6e4a92d0c87aa29c52035
Jonathan Feng <jonathan.feng@msichicago.org>
2019-04-01 11:37:10 -0500

    MMTR-47 close kicks off the reconnect loop

--- client/src/js/view/components/settings/settings-general.js
+++ client/src/js/view/components/settings/settings-general.js
@@ -4,5 +4,5 @@ export const SettingsGeneral = ({ actions }) =>
     <div class="settings_content">
         <button type='button' class='button' onclick={() => actions.system.shutdown()}>SHUTDOWN</button>
         <button type='button' class='button' onclick={() => actions.system.reboot()}>REBOOT</button>
-        <button type='button' class='button' onclick={() => actions.stream.reconnect()}>RESTART STREAM</button>
+        <button type='button' class='button' onclick={() => actions.stream.close()}>RESTART STREAM</button>
     </div>;
